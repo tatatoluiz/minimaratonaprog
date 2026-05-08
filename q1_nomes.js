00280d6552cc2c82b5973e95a6971c0482f450f5
Luiz Otávio Gaike Biulchi <luizotaviogb@outlook.com>
2026-05-08 15:38:35 -0300

    e os guri do else if

---
+++ q1_nomes.js
@@ -0,0 +1,28 @@
+function solucao(n){
+    let vetor=[]
+    for(i=1;i<=n;i++){
+        if(i%3===0){
+            if(i%5===0){
+                vetor.push("FizzBuzz")
+            }else{
+                vetor.push("Fizz")
+            }
+            
+        }else
+        if(i%5===0){
+            vetor.push("Buzz")
+            
+        }else
+        if(i%7===0){
+            vetor.push("Boom")
+            
+        }else
+        vetor.push(i)
+    }
+    console.log(vetor)
+}
+
+// Sua solução aqui
+
+let resultado = "Olá, mundo!";
+document.getElementById("saida").innerHTML = resultado;
