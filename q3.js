b032bf43aae6e5bb9dee2462c5a66c107a7485d3
José Eduardo Nicolodi de Mesquita <joseeduardonicolodidemesquita@gmail.com>
2026-05-15 14:57:39 -0300

    Questão 3 feita

--- q3.js
+++ q3.js
@@ -1,3 +1,17 @@
-function solucao(texto){
+function solucao(vetor){
     
+    let tamanho = vetor.length;
+
+    for(i = tamanho-1; i >= 0; i--){
+
+        vetor.push(vetor[i]);
+
+    }
+
+    vetor.splice(0, tamanho);
+
+    console.log(vetor);
+
 }
+
+solucao([1, 2, 3, 4, 5, 6]);
